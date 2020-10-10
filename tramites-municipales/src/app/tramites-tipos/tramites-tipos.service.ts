50943fb7e7c15df56e58be2044978797261e8e28
Erikgach <69018839+Erikgach@users.noreply.github.com>
2020-10-09 23:31:20 -0600

    Se Limpia el codigo
    Limpieza de codigo y arreglo de algunos errores

--- tramites-municipales/src/app/tramites-tipos/tramites-tipos.service.ts
+++ tramites-municipales/src/app/tramites-tipos/tramites-tipos.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+
 import { Observable } from 'rxjs';
+
 import { TramitesTipos } from './tramites-tipos';
 
 @Injectable({
